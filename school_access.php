c14afd1b1c199e7ccf2763c5f56a99b9c1a9873f
Habtamu Abd <habtamubefekadu20@gmail.com>
2026-03-23 08:25:14 +0300

    Add school class and access script with message method

---
+++ school_access.php
@@ -0,0 +1,13 @@
+<?php
+// //require "student.php";
+// $school = new practice\school();
+// $school->message();
+
+// use practice\school;
+// $school = new school();
+// $school->message();
+
+use practice\school as sch;
+$school = new sch();
+$school->message();
+?> 
---
+++ stundet.php
@@ -0,0 +1,11 @@
+<?php
+namespace practice;
+class school{
+   public function message(){
+    echo "Welcome to our school";
+   }
+
+    
+}
+
+?>
